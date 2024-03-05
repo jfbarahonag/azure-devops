d451e2e775f2f4e104641b33d50f17d506f6c423
jfbarahonag <jfbarahonag@unal.edu.co>
2024-03-04 21:30:36 -0500

    file_crud file has been modified to be a static class

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-from utils import AzureDevops
-from os import getenv
+from utils import AzureDevops, FileCRUD
+from os import getenv, getcwd, path
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,10 +9,23 @@ def main():
   PAT = getenv('PAT') # admin token
   organization_name = "jfbarahonagUN"
   project_name = "Wiki"
-  
+
   azure_devops = AzureDevops(organization_name, project_name, PAT)
   
-  wiki_pages = azure_devops.get_wiki().get_all_wiki_pages(recursive=True, include_content=True, cached=True)
+  wiki_pages = azure_devops\
+                          .get_wiki()\
+                          .get_all_wiki_pages(
+                            recursive=True, 
+                            include_content=True, 
+                            cached=True
+                          )
+  cwd = path.join(getcwd(), 'wiki')
+  for page in wiki_pages:
+    fp = path.join(
+      cwd, 
+      page['gitItemPath'][1:] ## remove /
+    )
+    FileCRUD.create(page['content'], fp, force=True)
   
     
 if __name__ == '__main__':
--- utils/file/file_crud.py
+++ utils/file/file_crud.py
@@ -1,34 +1,39 @@
 import os
 
 class FileCRUD:
-    def __init__(self, filepath):
-        self.filepath = filepath
-    
-    def create(self, content, filepath=None):
-        filepath = filepath or self.filepath
-        if os.path.exists(filepath):
+    @staticmethod
+    def create(content, filepath, force=False):
+        directory = os.path.dirname(filepath)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        if os.path.exists(filepath) and force == False:
             return False
+        if os.path.isdir(filepath):
+          return True
+
         with open(filepath, 'w') as file:
             file.write(content)
+
         return True
     
-    def read(self, filepath=None):
-        filepath = filepath or self.filepath
+    @staticmethod
+    def read(filepath):
         if not os.path.exists(filepath):
             return None
         with open(filepath, 'r') as file:
             return file.read()
     
-    def update(self, content, filepath=None):
-        filepath = filepath or self.filepath
+    @staticmethod
+    def update(content, filepath):
         if not os.path.exists(filepath):
             return False
         with open(filepath, 'w') as file:
             file.write(content)
         return True
     
-    def delete(self, filepath=None):
-        filepath = filepath or self.filepath
+    @staticmethod
+    def delete(filepath):
         if not os.path.exists(filepath):
             return False
         os.remove(filepath)
