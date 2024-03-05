983e53df3f257a6e67e638a646f8e0a7e0fc5e69
jfbarahonag <jfbarahonag@unal.edu.co>
2024-03-04 23:02:37 -0500

    main file has been edited to load to storage account

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from utils import AzureDevops, FileCRUD
+from utils import AzureDevops, FileCRUD, AzureStorageCRUD
 from os import getenv, getcwd, path
 
 from dotenv import load_dotenv
@@ -7,26 +7,45 @@ load_dotenv()
 def main():
   ## Connection data
   PAT = getenv('PAT') # admin token
+  STORAGE = getenv('STORAGE')
   organization_name = "jfbarahonagUN"
   project_name = "Wiki"
+  option = "cloud"
 
+  print("1. Get data from azure devops")
+  
+  ## Get pages from Wiki
   azure_devops = AzureDevops(organization_name, project_name, PAT)
-  
   wiki_pages = azure_devops\
-                          .get_wiki()\
-                          .get_all_wiki_pages(
-                            recursive=True, 
-                            include_content=True, 
-                            cached=True
-                          )
-  cwd = path.join(getcwd(), 'wiki')
-  for page in wiki_pages:
-    fp = path.join(
-      cwd, 
-      page['gitItemPath'][1:] ## remove /
-    )
-    FileCRUD.create(page['content'], fp, force=True)
+                  .get_wiki()\
+                  .get_all_wiki_pages(
+                    recursive=True, 
+                    include_content=True, 
+                    cached=True
+                  )[:-1] ## remove root (/) file
   
+  print("2. Upload to storage account")
+  
+  ## upload to file
+  if (option == "local"):
+    cwd = path.join(getcwd(), 'wiki')
+    for page in wiki_pages:
+      fp = path.join(
+        cwd, 
+        page['gitItemPath'][1:] ## remove /
+      )
+      FileCRUD.create(page['content'], fp, force=True)
+  else:
+    azst = AzureStorageCRUD(
+      account_url=STORAGE,
+      use_managed_identity=True,
+    )
+    
+    azst.upload_blobs(
+      "wiki", 
+      [(page['gitItemPath'][1:], page['content']) for page in wiki_pages]
+    )
+    
     
 if __name__ == '__main__':
   main()
